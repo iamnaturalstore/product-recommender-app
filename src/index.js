6a58cb50314d542baa72f90c974ff9e44cd6e41b
Ross Anderson <rossanderson@Mac.modem>
2025-07-24 13:35:15 +1000

    chatGPT version

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react';
 import { getSessionToken } from '@shopify/app-bridge-utils';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider as PolarisProvider } from '@shopify/polaris';
@@ -16,14 +15,6 @@ const host = new URLSearchParams(window.location.search).get('host');
 root.render(
   <BrowserRouter>
     <PolarisProvider>
-      <AppBridgeProvider
-        config={{
-          apiKey: 'YOUR_SHOPIFY_API_KEY', // we'll dynamically inject this later
-          host: host,
-          forceRedirect: true
-        }}
-      >
-        <App />
       </AppBridgeProvider>
     </PolarisProvider>
   </BrowserRouter>
