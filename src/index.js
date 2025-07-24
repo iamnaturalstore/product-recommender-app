46d8d899b1856a6fcee087e30efefdc4a28e5ddf
Ross Anderson <rossanderson@Mac.modem>
2025-07-24 13:46:42 +1000

    chatGPT v2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AppProvider as PolarisProvider } from '@shopify/polaris';
+
+import '@shopify/polaris/build/esm/styles.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <PolarisProvider>
         <App />
       </PolarisProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
