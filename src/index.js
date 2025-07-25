8f13a757a2dbe278baf04cc525443d316904932a
Ross Anderson <rossanderson@Mac.modem>
2025-07-25 11:37:20 +1000

    indexjs changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // ✅ CHANGE THIS
 import { AppProvider as PolarisProvider } from '@shopify/polaris';
-
 import '@shopify/polaris/build/esm/styles.css';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter> {/* ✅ Must be HashRouter inside Shopify Admin */}
       <PolarisProvider>
         <App />
       </PolarisProvider>
