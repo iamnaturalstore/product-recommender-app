d2dcffdadb9b2d2f65e9b7164c31308f7704cf84
Ross Anderson <rossanderson@Mac.modem>
2025-07-25 11:21:06 +1000

    Use HashRouter for Shopify compatibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppProvider as PolarisProvider } from '@shopify/polaris';
 
 import '@shopify/polaris/build/esm/styles.css';
@@ -9,10 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PolarisProvider>
         <App />
       </PolarisProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
